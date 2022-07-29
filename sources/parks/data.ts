00d3c31b2e0af576918e8753c1c29242125496bc
Steve Cresswell <229672+cressie176@users.noreply.github.com>
2022-07-29 11:00:29 +0100

    Propose alternative source formats

---
+++ sources/parks/data.ts
@@ -0,0 +1,77 @@
+import { entity, value, omit } from "../../src/dsl";
+
+export default {
+  data: [
+    [
+      entity({
+        name: [value("Devon Clifs")],
+        code: [value("DC")],
+        covidRestrictions: [omit("2021-06-01"), value(true)],
+        address: [
+          entity({
+            addressLine1: [value("Devon Cliffs Holiday Park")],
+            addressLine2: [value("Devon Cliffs Holiday Park")],
+            town: [value("Exmouth")],
+            county: [value("Devon")],
+            postcode: [value("EX8 5BT")],
+          }),
+        ],
+        telephone: [value("01395226226")],
+        coordinates: [
+          entity({
+            latitude: [value(50.615)],
+            longitude: [value(3.3666)],
+          }),
+        ],
+        dogFriendly: [value(true)],
+        season: [
+          [
+            entity({
+              guests: [
+                entity({
+                  start: [value("2022-03-11")],
+                  end: [value("2022-11-07")],
+                }),
+              ],
+              touring: [
+                entity({
+                  start: [value("2022-03-11")],
+                  end: [value("2022-11-07")],
+                }),
+              ],
+              owners: [
+                entity({
+                  start: [value("2022-03-11")],
+                  end: [value("2022-11-07")],
+                }),
+              ],
+            }),
+            entity(
+              {
+                guests: [
+                  entity({
+                    start: [value("2023-03-11")],
+                    end: [value("2023-11-07")],
+                  }),
+                ],
+                touring: [
+                  entity({
+                    start: [value("2023-03-11")],
+                    end: [value("2023-11-07")],
+                  }),
+                ],
+                owners: [
+                  entity({
+                    start: [value("2023-03-11")],
+                    end: [value("2023-11-07")],
+                  }),
+                ],
+              },
+              "2022-12-01"
+            ),
+          ],
+        ],
+      }),
+    ],
+  ],
+};
---
+++ src/dsl.ts
@@ -0,0 +1,5 @@
+export function entity(attributes: Record<string, any>, from?: string | Date) {}
+
+export function value(value: string | number | boolean | Date, from?: string | Date) {}
+
+export function omit(from: string | Date) {}
